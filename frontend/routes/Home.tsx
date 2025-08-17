15cb5f1a71002e97dcb3ccd63f2e1082e8db11a2
Mehdi Ounejjar <53841190+shonjmoj@users.noreply.github.com>
2025-08-17 20:25:11 +0100

    docs: format

--- frontend/routes/Home.tsx
+++ frontend/routes/Home.tsx
@@ -1,15 +1,15 @@
-import Chat from "@/frontend/components/Chat";
-import { v4 as uuidv4 } from "uuid";
-import { useAPIKeyStore } from "../stores/APIKeyStore";
-import { useModelStore } from "../stores/ModelStore";
-import APIKeyForm from "../components/APIKeyForm";
-import { Key } from "lucide-react";
+import Chat from '@/frontend/components/Chat';
+import { v4 as uuidv4 } from 'uuid';
+import { useAPIKeyStore } from '../stores/APIKeyStore';
+import { useModelStore } from '../stores/ModelStore';
+import APIKeyForm from '../components/APIKeyForm';
+import { Key } from 'lucide-react';
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../components/ui/card";
+} from '../components/ui/card';
 
 export default function Home() {
   const hasRequiredKeys = useAPIKeyStore((state) => state.hasRequiredKeys());
@@ -21,12 +21,12 @@ export default function Home() {
 
   if (!hasRequiredKeys)
     return (
-      <div className="flex flex-col items-center justify-center w-full h-full max-w-sm md:max-w-3xl py-20 mx-auto">
-        <Card className="w-full max-w-2xl mx-auto">
+      <div className='flex flex-col items-center justify-center w-full h-full max-w-sm md:max-w-3xl py-20 mx-auto'>
+        <Card className='w-full max-w-2xl mx-auto'>
           <CardHeader>
-            <div className="flex items-center gap-2">
-              <Key className="h-5 w-5" />
-              <CardTitle className="">
+            <div className='flex items-center gap-2'>
+              <Key className='h-5 w-5' />
+              <CardTitle className=''>
                 Add Your API Keys To Start Chatting
               </CardTitle>
             </div>
