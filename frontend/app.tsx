2484a5d50e15b7e9ed894f76b4c3227dc22efa8b
Mehdi Ounejjar <53841190+shonjmoj@users.noreply.github.com>
2025-08-17 20:21:27 +0100

    docs: format

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import ChatLayout from "./ChatLayout";
-import Home from "./routes/Home";
-import Index from "./routes/Index";
-import Thread from "./routes/Thread";
+import { BrowserRouter, Route, Routes } from 'react-router';
+import ChatLayout from './ChatLayout';
+import Home from './routes/Home';
+import Index from './routes/Index';
+import Thread from './routes/Thread';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="chat" element={<ChatLayout />}>
+        <Route path='/' element={<Index />} />
+        <Route path='chat' element={<ChatLayout />}>
           <Route index element={<Home />} />
-          <Route path=":id" element={<Thread />} />
+          <Route path=':id' element={<Thread />} />
         </Route>
-        <Route path="*" element={<p> Not found </p>} />
+        <Route path='*' element={<p> Not found </p>} />
       </Routes>
     </BrowserRouter>
   );
